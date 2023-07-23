1848495a384a8c39ce8c239e9f324e7d89f6bfca
ranadheer2012 <110771454+ranadheer2012@users.noreply.github.com>
2023-07-23 11:37:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var splash
+
+function preload(){
+splash = loadImage("assets/trouble.gif")
+
+}
+
+
+function setup(){
+    createCanvas(windowWidth,windowHeight)
+
+
+}
+
+
+function draw(){
+background(splash)
+
+    
+}
