5fce078727121f3f6f7560be9d7b6ea06e1f6976
ranadheer2012 <110771454+ranadheer2012@users.noreply.github.com>
2023-08-13 10:53:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var splash
 var gameState = "wait"
-var playbutton,playerimg, soundonbutton, soundoffbutton, level1bgimg, level1bg, level2bgimg, level2bg
+var playbutton, playerimg, invisibleground, soundonbutton, soundoffbutton, level1bgimg, level1bg, level2bgimg, level2bg
 var health = 0
 var maxHealth = 400
 var playerimg1
@@ -9,18 +9,23 @@ var score1 = 0
 
 function preload() {
     splash = loadImage("assets/trouble.gif")
-    level1bgimg = loadImage("level1bg.jpg")
-    playerimg=loadAnimation("assets/player/player1.png","assets/player/player2.png","assets/player/player3.png","assets/player/player4.png")
-    playerimg1=loadImage("assets/player/player1.png")
+    level1bgimg = loadImage("assets/level1bg.jpg")
+    playerimg = loadAnimation("assets/player/player1.png", "assets/player/player2.png", "assets/player/player3.png", "assets/player/player4.png")
+    playerimg1 = loadImage("assets/player/player1.png")
 
+
+    // enemy Images
+   enemy1img = loadImage("assets/enemy/enemy1.png") 
+   enemy2img = loadImage("assets/enemy/enemy2.png")
+   enemy3img = loadImage("assets/enemy/enemy3.png")
 }
 
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
 
-    playbutton = createImg("startbutton.png")
-    playbutton.position(width / 2 - 100, height / 2 + height / 4)
+    playbutton = createImg("assets/startbutton.png")
+    playbutton.position(width / 2 - 100, height / 2.5 + height / 4)
 
     // soundonbutton = createImg("soundOn.png")
     // soundonbutton.position(playbutton.x-200,height/2+height/5)
@@ -31,18 +36,21 @@ function setup() {
     // soundoffbutton.size(150,150)
 
 
-    level1bg = createSprite(width / 2, height / 2, width, height)
+    level1bg = createSprite(width / 2, height / 1.4, width, height)
     level1bg.addImage(level1bgimg)
     level1bg.scale = 1.95
     level1bg.visible = false
 
 
-    player=createSprite(100,height-150,50,50)
+    player = createSprite(150, height - 200, 50, 50)
     // player.addImage(playerimg1)
-    player.addAnimation(playerimg)
-    player.scale=0.8
-    player.visible=false
+    player.addAnimation("walk", playerimg)
+    player.scale = 0.8
+    player.visible = false
 
+
+    invisibleground = createSprite(width / 2, height - 100, width, 10)
+    // invisibleground.visible = false
 
 }
 
@@ -61,34 +69,49 @@ function draw() {
         background(level1bgimg)
         level1bg.visible = true
         // player.visible=true
+        spawnEnemies()
+
 
         level1bg.velocityX += -2
         if (level1bg.x <= 0) {
-            level1bg.x =level1bg.width / 2
+            level1bg.x = level1bg.width / 2
         }
 
-        player.visible=true
+        player.visible = true
 
+        if (keyDown("UP_ARROW")) {
+            player.y -= 2
+        }
+        // collectibles()
+
+        if (keyDown("DOWN_ARROW")) {
+            player.y += 2
+        }
+
+        if (keyDown("space")) {
+            player.velocityY = -10
+        }
+
+        player.velocityY +=0.8
+
+        player.collide(invisibleground)
 
     }
 
     drawSprites()
 
-    if(gameState=="level1"){
+    if (gameState == "level1") {
         textSize(50)
         fill("black")
-        stroke(255,0,0)
+        stroke(255, 0, 0)
         strokeWeight(2)
-        text("LEVEL 1",width/2-100,80)
+        text("LEVEL 1", width / 2 - 100, 80)
         healthlevel1()
 
     }
 }
 
 
-function spawnEnemies(){
-
-}
 
 
 function healthlevel1() {
@@ -101,4 +124,33 @@ function healthlevel1() {
     noStroke();
     fill("red");
     rect(10, 10, map(health, 0, maxHealth, 0, 200), 20);
+}
+
+
+function spawnEnemies(){
+
+if (frameCount % 100 ===0){
+    var enemy= createSprite(width,height-200)
+    enemy.velocityX=-6
+     //generate random obstacles
+var rand=Math.round(random(1,3))
+switch(rand){
+    case 1: enemy.addImage(enemy1img);
+    enemy.scale=1
+    break;
+case 2: enemy.addImage(enemy2img);
+enemy.scale=0.5
+    break;
+case 3: enemy.addImage(enemy3img);
+enemy.scale=1
+    break;
+    default: break;
+
+}
+
+enemy.debug=true
+
+}
+
+    
 }
